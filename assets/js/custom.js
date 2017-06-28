734eec518c5fe1d2bb960417281fae065e5fc95a
Dhruv <pythonbrilliant@gmail.com>
2017-06-28 10:11:05 +0530

    Added MAP

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,20 @@
 /* Add here all your JS customizations */
+
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(1.2854739,103.8479511),
+        zoom: 15,
+        
+    }
+
+
+var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(1.2854739,103.8479511),
+          map: map
+        });
+
+
+}
